d996ae06cf5268459dd27b323c3f0a1db918238c
Karsten Fleischer <karsten.fleischer@hrs.de>
2023-03-13 18:00:12 +0100

    prepare for USB MIDI

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <MIDI.h>
 #include <Adafruit_MCP23X17.h>
-#include <TeensyThreads.h>
+#include <USBHost_t36.h>
 
 #include "midi.h"
 
@@ -13,28 +13,33 @@ auto MODE_NUM = 3; // +1
 auto BASE_NOTE = 48;
 auto POLY_MODE_NOTE_ADD_L = 12;
 auto POLY_MODE_NOTE_ADD_R = 24;
-auto MIC_BOOST = 0xAAC4; // 0xFFC4 = 48dB, 0x99C4 = 30dB, 0x88C4 = 0dB
-auto DAC_VOL = 0xA2A2; // 左右通道输出音量 A0 = 0dB, A2 = 1.5dB, A4 = 3dB
-
-auto slider_movement_detect = 256;
-
 
 struct MySettings : public midi::DefaultSettings
 {
-  static const unsigned SysExMaxSize = 16;
-  static const bool UseRunningStatus = false;
-  static const bool Use1ByteParsing = true;
-  static const long BaudRate = 31250;
+    static const unsigned SysExMaxSize = 16;
+    static const bool UseRunningStatus = false;
+    static const bool Use1ByteParsing = true;
+    static const long BaudRate = 31250;
 };
 
-
+USBHost myusb;
+USBHub hub1(myusb);
+USBHub hub2(myusb);
+MIDIDevice midi1(myusb);
 MIDI_CREATE_CUSTOM_INSTANCE(HardwareSerial, Serial2, MIDI, MySettings);
 
-Adafruit_MCP23X17 mcp0;
-Adafruit_MCP23X17 mcp1;
+Adafruit_MCP23X17 mcp_left;
+Adafruit_MCP23X17 mcp_right;
 
 void setup() {
     Serial.begin(115200);
+
+    myusb.begin();
+    midi1.setHandleNoteOn(handleNoteOn);
+    midi1.setHandleControlChange(handleControlChange);
+
+    usbMIDI.setHandleNoteOn(handleNoteOn);
+    usbMIDI.setHandleControlChange(handleControlChange);
 
     MIDI.begin(MIDI_CHANNEL_OMNI);
     MIDI.setHandleNoteOn(handleNoteOn);
